378597341c9b90b5e1c41885f31de162caebfba2
Mastah <mastah95@gmail.com>
2019-03-10 21:50:48 +0100

    Add Aes methods

--- src/Aes.py
+++ src/Aes.py
@@ -1,7 +1,38 @@
 from CipherBase import CipherBase
+from AesConstants import aes_sbox, aes_sbox_inv, aes_rcon, mix_col_mat
+import numpy as np
 
 
 class Aes(CipherBase):
-    
+
     def __init__(self):
+        self.key = b'\xFF\xFF\xFF\xFF'
+        self.sbox = aes_sbox
+        self.sbox_inv = aes_sbox_inv
+        self.rcon = aes_rcon
+
+    def sub_bytes(self, block):
+        return [aes_sbox[elem] for elem in block]
+
+    def shift_rows(self, block):
+        shifted = []
+        for i, column in enumerate(block):
+            shifted.append(np.roll(column, -i))
+        return np.asarray(shifted)
+
+    def mix_columns(self, block):
+        col_block = []
+        for i in range(0, 4):
+            col = block[i:i+16:4]
+            col = [int(elem, 0) for elem in col]
+            print(np.matmul(mix_col_mat, np.transpose(col)))
+
+    def add_round_key(self, block):
         pass
+
+    def round(self, block, isLast):
+        self.sub_bytes(block)
+        self.shift_rows(block)
+        if not isLast:
+            self.mix_columns(block)
+        self.add_round_key(block)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,25 @@
 from CipherBase import CipherBase
+from Aes import Aes
+import numpy as np
+
 
 cipherBase = CipherBase()
-print('Test')
+aes = Aes()
+
+list = [0x19, 0xa0, 0x9a, 0xe9,
+        0x3d, 0xf4, 0xc6, 0xf8,
+        0xe3, 0xe2, 0x8d, 0x48,
+        0xbe, 0x2b, 0x2a, 0x08]
+
+sub_byted = np.reshape([hex(x) for x in aes.sub_bytes(list)], (4, 4))
+
+
+shifted = aes.shift_rows(sub_byted)
+print(shifted)
+
+
+aes.mix_columns(shifted.flatten())
+
+
+
+
