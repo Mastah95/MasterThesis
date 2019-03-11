a2c66cf369ad4efca6b900b50e82d8ab933d4d64
Mastah <mastah95@gmail.com>
2019-03-11 18:49:31 +0100

    Reconcile array types, add key

--- src/Aes.py
+++ src/Aes.py
@@ -6,20 +6,28 @@ import numpy as np
 
 class Aes(CipherBase):
 
-    def __init__(self):
-        self.key = b'\xFF\xFF\xFF\xFF'
+    def __init__(self, key):
+        self.key = key if key else b'\xFF\xFF\xFF\xFF'
         self.sbox = aes_sbox
         self.sbox_inv = aes_sbox_inv
         self.rcon = aes_rcon
+        self.state = []
+    
+    def set_state(self, block):
+        print(block)
+        self.state = block
 
-    def sub_bytes(self, block):
-        return [aes_sbox[elem] for elem in block]
+    def print_state_hex(self):
+        print(np.reshape([hex(x) for x in self.state.flatten()], (4, 4)))
 
-    def shift_rows(self, block):
+    def sub_bytes(self):
+        self.state = np.reshape([aes_sbox[elem] for elem in self.state.flatten()], (4, 4))
+
+    def shift_rows(self):
         shifted = []
-        for i, column in enumerate(block):
+        for i, column in enumerate(self.state):
             shifted.append(np.roll(column, -i))
-        return np.asarray(shifted)
+        self.state = np.asarray(shifted)
 
     def galois_field_mult(self, a, b):
         p = 0
@@ -33,12 +41,13 @@ class Aes(CipherBase):
             b >>= 1
         return p
 
-    def mix_columns(self, block):
+    def mix_columns(self):
+        block = self.state.flatten()
         for i in range(0, 4):
             col = block[i:i+16:4]
-            col = [int(elem, 0) for elem in col]
             col_cpy = col.copy()
             gfm = self.galois_field_mult  # alias for method
+
             col[0] = gfm(col_cpy[0], mix_col_mult[0]) ^ gfm(col_cpy[3], mix_col_mult[1]) ^ \
                      gfm(col_cpy[2], mix_col_mult[2]) ^ gfm(col_cpy[1], mix_col_mult[3])
 
@@ -51,15 +60,17 @@ class Aes(CipherBase):
             col[3] = gfm(col_cpy[3], mix_col_mult[0]) ^ gfm(col_cpy[2], mix_col_mult[1]) ^ \
                      gfm(col_cpy[1], mix_col_mult[2]) ^ gfm(col_cpy[0], mix_col_mult[3])
 
-            block[i:i + 16:4] = [hex(x) for x in col]
-        print(np.reshape(block, (4, 4)))
+            block[i:i + 16:4] = col
 
-    def add_round_key(self, block):
-        pass
+        self.state = np.reshape(block, (4, 4))
 
-    def round(self, block, isLast):
-        self.sub_bytes(block)
-        self.shift_rows(block)
+        
+    def add_round_key(self, round_key):
+        self.state ^= round_key
+
+    def round(self, isLast):
+        self.sub_bytes(self.state)
+        self.shift_rows(self.state)
         if not isLast:
-            self.mix_columns(block)
-        self.add_round_key(block)
+            self.mix_columns(self.state)
+        self.add_round_key(self.self.state)
--- src/main.py
+++ src/main.py
@@ -2,23 +2,29 @@ from CipherBase import CipherBase
 from Aes import Aes
 import numpy as np
 
+plain_text = np.array([[0x32, 0x88, 0x31, 0xe0],
+                       [0x43, 0x5a, 0x31, 0x37],
+                       [0xf6, 0x30, 0x98, 0x07],
+                       [0xa8, 0x8d, 0xa2, 0x34]])
+
+key = [0x2b, 0x28, 0xab, 0x09,
+       0x7e, 0xae, 0xf7, 0xcf,
+       0x15, 0xd2, 0x15, 0x4f,
+       0x16, 0xa6, 0x88, 0x3c]
 
 cipherBase = CipherBase()
-aes = Aes()
+aes = Aes(key)
+aes.set_state(plain_text^np.reshape(key, (4, 4)))
 
-list = [0x19, 0xa0, 0x9a, 0xe9,
-        0x3d, 0xf4, 0xc6, 0xf8,
-        0xe3, 0xe2, 0x8d, 0x48,
-        0xbe, 0x2b, 0x2a, 0x08]
+aes.sub_bytes()
+aes.print_state_hex()
 
-sub_byted = np.reshape([hex(x) for x in aes.sub_bytes(list)], (4, 4))
+print("Shift")
+aes.shift_rows()
+aes.print_state_hex()
 
-
-shifted = aes.shift_rows(sub_byted)
-print(shifted)
-
-
-aes.mix_columns(shifted.flatten())
+aes.mix_columns()
+aes.print_state_hex()
 
 
 
